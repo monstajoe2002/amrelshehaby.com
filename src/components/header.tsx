aba799c314dc73160ef469f4355c980f539f7cdb
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-07 02:44:19 +0300

    edit header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,13 @@
+// TODO: add props to make the data dynamic
 const Header = () => {
   return (
     <header className="text-center mb-12">
       <h1>Dr. Amr El-Shehaby</h1>
-      <p className="text-lead">Neurosurgeon & Gamma Knife Specialist</p>
+      <p className="text-lead font-medium">M.B.B ch.MD. PhD</p>
+      <p className="text-lead">
+        Professor of Neurosurgery <br /> Facuty of Medicine - Ain Shams
+        University
+      </p>
     </header>
   );
 };
