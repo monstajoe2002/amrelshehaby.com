ad2a3f957d14d6ae4ae18a76e121ee4fed4044dc
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-07 14:23:32 +0300

    install unpic for image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), react()],
+  image: {
+    service: imageService(),
+  },
 });
