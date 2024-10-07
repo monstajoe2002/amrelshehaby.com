6417c37669da386665fef5410c5cfd22b2d7785e
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-07 17:21:22 +0300

    i18n: create a file for translation strings

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: "English",
+  ar: "العربية",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    //   TODO: Translate UI
+  },
+  ar: {
+    //   TODO: Translate UI
+  },
+} as const;
