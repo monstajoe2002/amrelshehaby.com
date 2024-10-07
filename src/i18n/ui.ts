2d998cdf4442d6f25dedf7cc16895078c85cd033
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-07 22:16:46 +0300

    add some translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,11 +7,30 @@ export const defaultLang = "en";
 export const showDefaultLang = false;
 export const ui = {
   en: {
-    //   TODO: Translate UI
-    "header.title": "",
+    home: {
+      header: {
+        title: "Dr. Amr El-Shehaby",
+        subtitle:
+          "M.B.B ch.MD. PhD | Professor of Neurosurgery | Faculty of Medicine - Ain Shams University",
+      },
+      body: {
+        heading: "Expertise in Stereotactic Radiosurgery",
+      },
+    },
   },
   ar: {
-    //   TODO: Translate UI
-    "header.title": "",
+    home: {
+      header: {
+        title: "أ.د/ عمرو الشهابي",
+        subtitle:
+          "دكتوراه جراحة المخ والأعصاب والعمود الفقري | أستاذ بكلية الطب - جامعة عين شمس | استشاري الجراحة اﻹشعاعية - معهد ناصر",
+      },
+      body: {
+        heading: "إستشاري جراحة المخ والأعصاب والجراحة الإشعاعية",
+        body: `
+        هو استشاري جراحة المخ و الأعصاب واستاذ بقسم جراحة الأعصاب و العمود الفقري بجامعة عين شمس . ويعمل د. عمرو الشهابي  في مركز الجاما نايف منذ البداية في 2001. وقد تدرب على يد البروفيسور جيريمي جانز، وهو رائد في مجال الجراحة الإشعاعية بإستخدام الجاما نايف. وكانت أطروحته لنيل درجة الدكتوراة هي الأولى  في مجال الجراحة الإشعاعية في مصر وهو عضو فاعل في العديد من الجمعيات العلمية العالمية منها جمعية ليكسل جاما نايف الدولية  والجمعية الدولية للجراحة الإشعاعية المجسمة. و يعتبر د/عمرو من رواد الجراحة الإشعاعية بمصروقد نشر في  العديد من الدوريات المتخصصة و على رأسها مجلة جراحة الأعصابJournal of Neurosurgery
+        `,
+      },
+    },
   },
 } as const;
