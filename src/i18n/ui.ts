4ebb63493c86d80c072559dcb167c9fe5f4181ca
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-08 18:22:30 +0300

    translate gamma knife page to arabic

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -51,6 +51,57 @@ export const ui = {
       "Trigeminal neuralgia",
     ],
     "home.gamma-knife-link": "Learn More about Gamma Knife",
+    "gamma-knife.header.title": "Gamma Knife Radiosurgery",
+    "gamma-knife.header.subtitle":
+      "Cutting-edge Technology for Brain Disorders",
+    "gamma-knife.conditions-treated.title": "Conditions Treated",
+    "gamma-knife.conditions-treated": [
+      "Brain tumors (benign and malignant)",
+      "Arteriovenous malformations (AVMs)",
+      "Acoustic neuroma (vestibular schwannomas)",
+      "Meningiomas",
+      "Pituitary adenomas",
+      "Trigeminal neuralgia",
+      "Brain metastases",
+      "Chondrosarcomas and Chordomas",
+      "Craniopharyngiomas",
+      "Gliomas",
+      "Paragangliomas",
+      "Cavernous hemangiomas",
+      "Certain cases of epilepsy",
+    ],
+    "gamma-knife.what-is-gk": "What is Gamma Knife Radiosurgery?",
+    "gamma-knife.what-is-gk.description":
+      "Gamma Knife is a non-invasive, cutting-edge technology used to treat brain disorders without the need for traditional open surgery. It uses highly focused gamma rays to precisely target and treat areas within the brain, providing an effective, safe, and less painful alternative for patients with various conditions.",
+    "gamma-knife.what-is-gk.key-benefits.title": "Key Benefits",
+    "gamma-knife.what-is-gk.key-benefits": [
+      "Non-invasive: No incisions, no scars",
+      "Outpatient Procedure: Many patients return home the same day",
+      "Quick Recovery: Minimal downtime, allowing you to return to your normal activities faster",
+      "Precision: Designed to treat even the most delicate areas in the brain",
+    ],
+    "gamma-knife.treatment-process.title": "Treatment Process",
+    "gamma-knife.treatment-process": [
+      {
+        step: "1. Frame Fixation",
+        text: "A lightweight frame is attached to the patient's head to ensure precise targeting during treatment.",
+      },
+      {
+        step: "2. MRI Imaging",
+        text: "Detailed MRI scans are taken to create a 3D map of the brain and precisely locate the treatment area.",
+      },
+      {
+        step: "3. Treatment Planning",
+        text: "Our team of experts uses advanced software to create a personalized treatment plan based on the MRI images.",
+      },
+      {
+        step: "4. Treatment Delivery",
+        text: "The patient lies comfortably while the Gamma Knife device delivers precise radiation beams to the target area.",
+      },
+    ],
+    "gamma-knife.treatment-process.end":
+      "Our experienced team will guide you through each step, ensuring your comfort and understanding throughout the process.",
+    "gamma-knife.back": "Back to Dr.Amr's Profile",
     footer: {
       title: "Contact Dr. Amr El-Shehaby",
       address:
@@ -107,7 +158,57 @@ export const ui = {
       "ألم العصب الثلاثي التوائم",
     ],
     "home.gamma-knife-link": "للمزيد عن الجاما نايف",
+    "gamma-knife.header.title": 'الجراحة الإشعاعية من "جاما نايف"',
+    "gamma-knife.header.subtitle":
+      "علاج لأورام المخ و الوحمات و العيوب الخلقية الدموية الدماغية بدون فتح",
 
+    "gamma-knife.conditions-treated.title": "الحالات المعالجة",
+    "gamma-knife.conditions-treated": [
+      "أورام الدماغ (الحميدة والخبيثة)",
+      "التشوهات الشريانية الوريدية (AVMs)",
+      "الورم الشفاني السمعي (ورم العصب السمعي)",
+      "الأورام السحائية",
+      "الأورام الغدية النخامية",
+      "ألم العصب الثلاثي التوائم",
+      "النقائل الدماغية",
+      "الأورام الغضروفية والورم الحبلي",
+      "الورم القحفي البلعومي",
+      "الأورام الدبقية",
+      "الأورام الجيبية (الباراغانغليوما)",
+      "الأورام الوعائية الكهفية",
+      "بعض حالات الصرع",
+    ],
+
+    "gamma-knife.what-is-gk": 'ما هي "الجراحة الإشعاعية" بالجاما نايف؟',
+    "gamma-knife.what-is-gk.description":
+      "الجاما نايف ترجمتها الحرفية هي مشرط جاما بس فالحقيقة هي مش مشرط بالمعنى المتعارف عليه. يعني الدكتور مش بيعمل أي جروح فعلية ولكن بيتم توجيه وتركيز الإشعاع بدقة عالية وبشكل مركز على المنطقة االلي عايزين نعالجها. والعلاج بيكون بدون ألم و كمان بيسمح بعودة المريض لحياته الإعتيادية في اليوم التالي للعلاج.",
+    "gamma-knife.what-is-gk.key-benefits.title":
+      "ما هي مزايا الجراحة الإشعاعية بإستخدام جاما نايف؟",
+    "gamma-knife.what-is-gk.key-benefits": [
+      "جاما نايف أداة مصممة خصيصا لجراحة الأعصاب وعلاج اضطرابات المخ.",
+      "الورم بيوصله جرعة عالية من الإشعاع بأقل خطر ممكن على الأنسجة المجاورة.",
+      "الإشعاع نفسه غير مؤلم.",
+      "فترة الاستشفاء والنقاهة قصيرة والمريض بيروح البيت في نفس اليوم. ويمكن للمرضى استئناف أنشطتهم السابقة في اليوم التالي في أغلب الأحوال.",
+      "الجاما نايف ممكن تعالج الحالات التي لايمكن التعامل معها جراحياً و بتدي الأمل للمرضى الذين كانوا يعتبرون سابقا ليس لهم علاج أو في أفضل التقديرات العلاج الجراحي كان هيبقى فيه خطورة عالية للمرضى دول.",
+    ],
+    "gamma-knife.treatment-process.title": "ما هي خطوات العلاج؟",
+    "gamma-knife.treatment-process": [
+      {
+        step: "1- تثبيت الحلقة المعدنية",
+      },
+      {
+        step: "2-	تصوير المريض بالرنين المغناطيسي",
+      },
+      {
+        step: "3-	تحديد خطة العلاج",
+      },
+      {
+        step: "4-	تنفيذ العلاج",
+      },
+    ],
+    "gamma-knife.treatment-process.end":
+      "بيتولى فريق عمل مدرب رعاية المريض والإعتناء به أثناء المراحل دي.",
+    "gamma-knife.back": "الرجوع لموقع د.عمرو",
     footer: {
       title: "تواصل مع الدكتور عمرو الشهابي",
       address:
