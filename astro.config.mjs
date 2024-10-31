ab4d991acdff80c83c9581f2987b100a39e58d38
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-31 22:41:10 +0300

    integrate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,24 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { imageService } from "@unpic/astro/service";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  site: "https://www.amrelshehaby.com/",
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          ar: "ar-EG",
+        },
+      },
+    }),
+  ],
   image: {
     service: imageService(),
   },
